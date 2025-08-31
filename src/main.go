f239b6db994f21dbf2d39fcadb7f9e42e96b2129
haraamurti <haramurti@student.ub.ac.id>
2025-08-31 23:57:43 +0700

    second commit setuping the goorm and fiber

--- src/main.go
+++ src/main.go
@@ -2,8 +2,61 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-func main(){
-	fmt.Println("Hello world this is for nasa image library")
+var db *gorm.DB
+
+type NASAimg struct{
+	ID int  `json:"id"`
+	Title string
+ 	URL string `json:"url"`         // Image link
+    Author    string `json:"author"`
+    CreatedAt string `json:"created_at"`
 }
+
+func handlerhome(c *fiber.Ctx)error{
+	message := "hello and welcome oline"
+	return c.JSON(message)
+}
+
+func main(){
+	app := fiber.New()
+	app.Get("/",handlerhome)
+	fmt.Println("Hello world this is for NASA image library")
+
+	host := "localhost"
+	user := "postgres"
+	password := "yipikaye2123"
+	port := 5432
+	dbname := "valinenasa" //using database this
+	sslmode := "disable"
+
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=%s",
+    host, user, password, dbname, port, sslmode,)
+
+	var errConnectDb error
+
+	db, errConnectDb = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if errConnectDb != nil {
+		fmt.Println("Failed to connect to database !")
+	}
+
+	fmt.Println("Connected to database!")
+
+	errorlisten := app.Listen(":8282")
+	if errorlisten != nil {
+		fmt.Println("Failed to connect to route localhost8282")
+	}
+
+	db.AutoMigrate(&NASAimg{})
+
+
+
+}
+
