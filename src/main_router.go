4ec00588190baf2fe8808d326aaa510355fa3e10
haraamurti <haramurti@student.ub.ac.id>
2025-09-13 23:59:18 +0700

    adding notes for review

--- src/main_router.go
+++ src/main_router.go
@@ -23,7 +23,7 @@ func main(){
 	fmt.Println("Hello world this is for NASA image library")
 
 	dsn := "postgresql://postgres.xrsnptveunsdsnfcvxjz:yipikaye2123@aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres"
-	var errConnectDb error
+	var errConnectDb error //eeror handler for making db handler eerro and the type is error in golang, error is a type that if error occur it will be filledwith the interface , but if therea re no tthe value will be nil
 
 	db, errConnectDb = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if errConnectDb != nil {
@@ -33,8 +33,8 @@ func main(){
 
 	db.AutoMigrate(&model.NasaolineImage{},&model.User{})
 	fmt.Println("Connected to database!")
-	handler.DB = db
-	// Pass DB to handler package after db is initialized!
+	handler.DB = db//this will asign the gorm db connection in handler to use the detials and spesification in the handler.
+	//adding details for review
 
 	app.Get("/", handler.HandlerHome)
 	app.Get("/imgs/:id",handler.HandlerImgDetails)
